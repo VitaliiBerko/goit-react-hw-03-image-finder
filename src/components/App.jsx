53a43a637496dc33042d9f00ebe024782ee5f00d
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-12-30 14:11:05 +0200

    started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,45 @@
-import { Fragment } from "react";
-import { Component } from "react";
-import { Modal } from "./Modal/Modal";
-
+import { Fragment } from 'react';
+import { Component } from 'react';
+import { Modal } from './Modal/Modal';
+import axios from 'axios';
 
 export class App extends Component {
+  state = {
+    showModal: false,
+    images: null,
+  };
 
-  state ={
-    showModal: false
+  async componentDidMount() {
+    try {
+      const {
+        data: { hits },
+      } = await axios.get(
+        'https://pixabay.com/api/?q=cat&page=1&key=31315876-ebaad9cfb6f2dd991d80baf37&image_type=photo&orientation=horizontal&per_page=12'
+      );
+
+      return hits;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  toggleModal =()=> {
-    this.setState(({showModal})=> ({showModal: !showModal}))
-  }
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
   render() {
-    const {showModal} = this.state
+    const { showModal } = this.state;
     return (
       <Fragment>
-        <button type="button" onClick={this.toggleModal}>Open</button>
-        {showModal && <Modal onClose={this.toggleModal}/>}
+        {/* <Searchbar />
+        <ImageGallery />
+
+        <Loader />
+        <Button /> */}
+
+        {/* <button type="button" onClick={this.toggleModal}>Open</button>
+        {showModal && <Modal onClose={this.toggleModal}/>} */}
       </Fragment>
-      
     );
   }
-  
-};
+}
